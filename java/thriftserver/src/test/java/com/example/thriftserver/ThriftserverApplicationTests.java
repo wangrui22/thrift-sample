966e54ebdb86999db8f762e9642c59fe1d340506
wangrui3 <wangrui3@sensetime.com>
2020-09-25 01:04:52 +0800

    add java proj

---
+++ java/thriftserver/src/test/java/com/example/thriftserver/ThriftserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thriftserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThriftserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
